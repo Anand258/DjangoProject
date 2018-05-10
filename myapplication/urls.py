0f81a43cb8f847423dbf9c6fea390a4a16678e2d
Anand Singh <anand.singh@tivo.com>
2018-05-10 20:49:14 +0530

    resolved namespace issue

--- myapplication/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import include, url
-from django.contrib import admin
-
-urlpatterns = [
-    url(r'^mysite/', include('mysite.urls', namespace='mysite')),
-    url(r'^admin/', include(admin.site.urls)),
-]
